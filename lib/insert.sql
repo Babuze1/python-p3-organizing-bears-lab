9c1d89890853155ca2fe03c3a399795843aaae69
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-09-04 16:39:42 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+  ('Tabitha', 2, 'F', 'White', 'Friendly', 1),
+  ('Sergeant Brown', 7, 'M', 'Brown', 'Grumpy', 1),
+  ('Melissa', 4, 'F', 'Black', 'Playful', 1),
+  ('Grinch', 6, 'M', 'Green', 'Sneaky', 1),
+  ('Wendy', 8, 'F', 'White', 'Calm', 1),
+  ('unnamed', NULL, NULL, NULL, NULL, 0);
